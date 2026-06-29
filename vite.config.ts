5db6ffae460d640161e3c2f6411595e4d4466937
Md Sadique Inam <md.sadique32@gmail.com>
2026-06-29 11:58:39 +0530

    feat: initialize Tauri app with React and Tailwind CSS
    - Created a new Tauri project named "pixeltool".
    - Added necessary configuration files including Cargo.toml, tauri.conf.json, and build.rs.
    - Implemented a basic Rust command to greet users.
    - Set up React components and integrated Tauri API for invoking Rust commands.
    - Included Tailwind CSS for styling and created a custom theme with CSS variables.
    - Added various icons and images for the application.
    - Configured Vite for development with auto-imports and aliasing.

---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import AutoImport from "unplugin-auto-import/vite";
+
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vite.dev/config/
+export default defineConfig(async () => ({
+  plugins: [
+    react(),
+    tailwindcss(),
+    AutoImport({
+      include: [/\.[tj]sx?$/],
+      imports: ["react"],
+      dirs: [
+        "src/composables",
+        "resource/js/shared/composables",
+        "resource/js/shared/components",
+      ],
+      dts: true,
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@shared": fileURLToPath(
+        new URL("./resource/js/shared", import.meta.url),
+      ),
+      "@svgs": fileURLToPath(new URL("./resource/svgs", import.meta.url)),
+    },
+  },
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent Vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
